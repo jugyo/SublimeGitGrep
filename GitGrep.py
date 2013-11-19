0e4acc04ff40e75f7e6d329e90579d8f50a17396
Kazuyuki Kohno <kkohno@twitter.com>
2013-11-19 17:41:29 +0900

    improve

--- GitGrep.py
+++ GitGrep.py
@@ -9,28 +9,41 @@ class GitGrepCommand(sublime_plugin.WindowCommand):
         items = []
 
         try:
-            base_dir = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'])
-            base_dir = base_dir.decode('utf8', 'ignore').strip()
+            base_dir = self.window.folders()[0]
             os.chdir(base_dir)
             matches = subprocess.check_output(['git', 'grep', query]).strip()
             matches = matches.decode('utf8', 'ignore').split("\n")
             for m in matches:
-                file_name, line, match = m.split(":", 2)
-                items.append([match.strip(), ":".join([file_name, line])])
+                try:
+                    file_name, line, match = m.split(":", 2)
+                    items.append([match.strip()[0:20], ":".join([file_name, line])])
+                except ValueError as e:
+                    print(e)
         except subprocess.CalledProcessError as e:
             print(e)
 
         if len(items) == 0:
             self.window.show_quick_panel(['No result'], None)
         else:
-            def on_done(index):
-                if index < 0:
-                    return
-                line = matches[index]
+            def open_for(line, mode):
                 file_name, line_no, match = line.split(":", 2)
                 file_name = os.path.join(base_dir, file_name)
-                view = self.window.open_file(file_name + ':' + line_no, sublime.ENCODED_POSITION)
+                view = self.window.open_file(file_name, mode)
                 point = view.text_point(int(line_no) - 1 ,0)
                 regison = view.line(point)
                 view.show_at_center(regison)
-            self.window.show_quick_panel(items, on_done)
+
+            def on_done(index):
+                if index >= 0:
+                    open_for(matches[index], sublime.ENCODED_POSITION)
+                else:
+                    view = self.window.active_view()
+                    (_, tab_index) = view.window().get_view_index(view)
+                    if tab_index < 0:
+                        view.close()
+
+            def on_highlight(index):
+                if index >= 0:
+                    open_for(matches[index], sublime.TRANSIENT)
+
+            self.window.show_quick_panel(items, on_done, sublime.MONOSPACE_FONT, -1, on_highlight)
